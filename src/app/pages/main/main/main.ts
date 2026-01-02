feff437b3fac462bc16c42da58dd079acdc1ed40
AllaBoo <a.v.buteeva@gmail.com>
2026-01-02 16:27:46 +0300

    Добавлена секция с описанием зала.

--- src/app/pages/main/main/main.ts
+++ src/app/pages/main/main/main.ts
@@ -2,11 +2,14 @@ import { Component } from '@angular/core';
 import { Header } from '../../../../shared/header/header';
 import { Hero } from './hero/hero';
 import { Events } from './events/events';
+import { Overview } from './overview/overview';
 
 @Component({
   selector: 'app-main',
-  imports: [Header, Hero, Events],
+  imports: [Header, Hero, Events, Overview],
   templateUrl: './main.html',
   styleUrl: './main.css',
 })
-export class Main {}
+export class Main {
+  protected readonly titles = ['Зал\u00A0функционального тренинга'];
+}
